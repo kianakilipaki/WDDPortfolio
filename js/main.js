618f79b653bb23fd524afd74b80265e4ae92f682
Alani Gilbert <44738610+kianakilipaki@users.noreply.github.com>
2021-04-29 23:10:18 -0600

    week2

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
 // create array
-const links = [
-  {
+const links = [{
     label: "Week1 Notes",
     url: "assignments/week1/story_editor.html"
+  },
+  {
+    label: "Week2 Notes",
+    url: "assignments/week2/notes.html"
+  },
+  {
+    label: "Week2 Team Activity",
+    url: "assignments/week2/team-activity.html"
   }
 ]
 
 // fill list with array items
-var list;
+let list = '';
 links.forEach(link => {
-  list = "<li><a href='" + link['url'] + "' >" + link['label'] + "</a></li>";
+  list += "<li><a href='" + link['url'] + "' >" + link['label'] + "</a></li>";
 });
 document.getElementById("link-list").innerHTML = list;
