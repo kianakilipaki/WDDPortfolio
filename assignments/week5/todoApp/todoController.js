b47ecb57fc1eb4c822ae64ed909472adb5161b0a
Alani Gilbert <44738610+kianakilipaki@users.noreply.github.com>
2021-05-24 12:18:43 -0600

    lists

--- assignments/week5/todoApp/todoController.js
+++ assignments/week5/todoApp/todoController.js
@@ -6,16 +6,18 @@ class todoController {
   constructor(parentId) {
     this.parent = document.getElementById(parentId);
     this.Model = new todoModel();
-    this.View = new todoView(parentId);
+    this.View = new todoView(parent);
   }
 
   showTodoList() {
     const todos = this.Model.getAllTodos();
     this.View.renderTodoList(todos, this.parent);
   }
+
+  // submit task
 }
 
-const TodoController = new todoController();
+const TodoController = new todoController("lists");
 window.addEventListener("load", () => {
   TodoController.showTodoList();
 });
